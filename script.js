14f9363d144222f4ce240d8efe30d47afeeda45f
Henrincode <spellctro@gmail.com>
2025-07-14 16:56:12 -0300

    feat: criado campo para envio de nome e contato para o banco de dados

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
+// =====================================================
+// 🌍 VARIÁVEIS GLOBAIS
+// =====================================================
+
 const BANCO_CONTATOS = 'contatos'
 const contatosDados = dadosCarregar(BANCO_CONTATOS, [])
 
+// =====================================================
+// 📌 SELETORES DOM
+// =====================================================
+
+const contatos = document.querySelector('#contatos')
+
+// =====================================================
+// 🎯 EVENTOS
+// =====================================================
+
+contatos.innerHTML = `
+            <div class="contato">
+                <i class="foto bi bi-person-circle"></i>
+                <ul>
+                    
+                    <li class="nome">
+                        ${capitalize(maxString('aRTHr mArson', 13))}
+                    </li>
+                </ul>
+            </div>
+            <div class="contato">a</div>
+            <div class="contato">a</div>
+            <div class="contato">a</div>
+`
+
+// =====================================================
+// 🛠️ FUNÇÕES
+// =====================================================
+
+// CONEXÃO COM BANCO
 function dadosCarregar(banco, err) {
     if (!banco) {
         console.error('Banco de dados não informado')
@@ -22,6 +56,7 @@ function dadosSalvar(dados, banco) {
     localStorage.setItem(banco, dados)
 }
 
+// FUNÇÕES DE CONTATOS
 function contatosAdicionar(contato) {
     contatosDados.push(contato)
     dadosSalvar(contatosDados, BANCO_CONTATOS)
@@ -35,6 +70,25 @@ function contatosAlterar(contato) {
 
 function contatosApagar(contato) {
     const indice = contatosDados.findIndex(c => c.id == contato.id)
-    contatosDados.splice(indice, 1)
-    dadosSalvar(contatosDados, BANCO_CONTATOS)
+    if (indice !== -1) {
+        contatosDados.splice(indice, 1)
+        dadosSalvar(contatosDados, BANCO_CONTATOS)
+    }
+}
+
+// =====================================================
+// 🛠️ FUNÇÕES UTILITÁRIAS
+// =====================================================
+
+// Máximo de caracteres
+function maxString(string, max) {
+    return string.length <= max ? string : string.slice(0, max) + '...'
+}
+
+// CAIXA ALTA NA PRIMEIRA LETRA DE CADA PALAVRA
+function capitalize(string = '') {
+    return string.toLowerCase()
+        .split(' ')
+        .map(palavra => palavra.charAt(0).toUpperCase() + palavra.slice(1))
+        .join(' ')
 }
